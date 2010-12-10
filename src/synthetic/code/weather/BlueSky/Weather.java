4ce025e290364d1e413a3980de50e402ec945fca
Synthetic Code <Synthetic.Code@gmail.com>
2010-12-10 11:56:17 -0800

    First Commit : Empty Android project.

---
+++ src/synthetic/code/weather/BlueSky/Weather.java
@@ -0,0 +1,13 @@
+package synthetic.code.weather.BlueSky;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Weather extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
