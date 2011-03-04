6021883cf799946d3f18bfeea6e0d3cde11d9071
Synthetic Code <Synthetic.Code@gmail.com>
2011-03-04 11:30:05 -0800

    Changed forecast icons again.
    - Using a different icon set from http://xiao4.deviantart.com/art/tick-weather-icons-96294478
    
    - Added night icons
    
    - Added more icons to accurately display conditions
    
    - Note that the icon name in the xml feed does not always match the forecast for short
    
    Signed-off-by: Synthetic Code <Synthetic.Code@gmail.com>

--- src/synthetic/code/weather/BlueSky/ForecastData.java
+++ src/synthetic/code/weather/BlueSky/ForecastData.java
@@ -16,26 +16,48 @@ public class ForecastData {
 	private final static HashMap<String, Integer> iconHash = new HashMap<String, Integer>();
 	static {
 		// All Weather Underground icon names mapped to icons
-		iconHash.put("chanceflurries", R.drawable.ic_forecast_snow);
+		iconHash.put("chanceflurries", R.drawable.ic_forecast_flurries);
 		iconHash.put("chancerain", R.drawable.ic_forecast_rain);
-		iconHash.put("chancesleet", R.drawable.ic_forecast_snow);
+		iconHash.put("chancesleet", R.drawable.ic_forecast_sleet);
 		iconHash.put("chancesnow", R.drawable.ic_forecast_snow);
-		iconHash.put("chancetstorms", R.drawable.ic_forecast_thunderstorm);
+		iconHash.put("chancetstorms", R.drawable.ic_forecast_tstorm);
 		iconHash.put("clear", R.drawable.ic_forecast_clear);
 		iconHash.put("cloudy", R.drawable.ic_forecast_cloudy);
-		iconHash.put("flurries", R.drawable.ic_forecast_snow);
+		iconHash.put("flurries", R.drawable.ic_forecast_flurries);
 		iconHash.put("fog", R.drawable.ic_forecast_fog);
-		iconHash.put("hazy", R.drawable.ic_forecast_fog); // ??
-		iconHash.put("mostlycloudy", R.drawable.ic_forecast_cloudy);
-		iconHash.put("mostlysunny", R.drawable.ic_forecast_partly_sunny);
-		iconHash.put("partlycloudy", R.drawable.ic_forecast_partly_sunny);
-		iconHash.put("partlysunny", R.drawable.ic_forecast_partly_sunny);
+		iconHash.put("hazy", R.drawable.ic_forecast_hazy);
+		iconHash.put("mostlycloudy", R.drawable.ic_forecast_mostlycloudy);
+		iconHash.put("mostlysunny", R.drawable.ic_forecast_mostlysunny);
+		iconHash.put("partlycloudy", R.drawable.ic_forecast_partlycloudy);
+		iconHash.put("partlysunny", R.drawable.ic_forecast_partlysunny);
 		iconHash.put("rain", R.drawable.ic_forecast_rain);
-		iconHash.put("sleet", R.drawable.ic_forecast_snow);
+		iconHash.put("sleet", R.drawable.ic_forecast_sleet);
 		iconHash.put("snow", R.drawable.ic_forecast_snow);
 		iconHash.put("sunny", R.drawable.ic_forecast_clear);
-		iconHash.put("tstorms", R.drawable.ic_forecast_thunderstorm);
-		iconHash.put("tstorm", R.drawable.ic_forecast_thunderstorm);
+		iconHash.put("tstorms", R.drawable.ic_forecast_tstorm);
+		iconHash.put("tstorm", R.drawable.ic_forecast_tstorm);
+		
+		iconHash.put("nt_chanceflurries", R.drawable.ic_forecast_nt_flurries);
+		iconHash.put("nt_chancerain", R.drawable.ic_forecast_nt_rain);
+		iconHash.put("nt_chancesleet", R.drawable.ic_forecast_sleet);
+		iconHash.put("nt_chancesnow", R.drawable.ic_forecast_nt_snow);
+		iconHash.put("nt_chancetstorms", R.drawable.ic_forecast_nt_tstorm);
+		iconHash.put("nt_clear", R.drawable.ic_forecast_nt_clear);
+		iconHash.put("nt_cloudy", R.drawable.ic_forecast_nt_cloudy);
+		iconHash.put("nt_flurries", R.drawable.ic_forecast_nt_flurries);
+		iconHash.put("nt_fog", R.drawable.ic_forecast_nt_fog);
+		iconHash.put("nt_hazy", R.drawable.ic_forecast_nt_hazy);
+		iconHash.put("nt_mostlycloudy", R.drawable.ic_forecast_nt_mostlycloudy);
+		//iconHash.put("nt_mostlysunny", R.drawable.ic_forecast_nt_mostlysunny);
+		iconHash.put("nt_partlycloudy", R.drawable.ic_forecast_nt_partlycloudy);
+		//iconHash.put("nt_partlysunny", R.drawable.ic_forecast_nt_partlysunny);
+		iconHash.put("nt_rain", R.drawable.ic_forecast_nt_rain);
+		iconHash.put("nt_sleet", R.drawable.ic_forecast_sleet);
+		iconHash.put("nt_snow", R.drawable.ic_forecast_nt_snow);
+		iconHash.put("nt_sunny", R.drawable.ic_forecast_nt_clear);
+		iconHash.put("nt_tstorms", R.drawable.ic_forecast_nt_tstorm);
+		iconHash.put("nt_tstorm", R.drawable.ic_forecast_nt_tstorm);
+		
 		iconHash.put("unknown", R.drawable.ic_forecast_unknown);
 	}
 	
@@ -53,10 +75,11 @@ public class ForecastData {
 	
 	private static int convertIconToId(String icon) {
 		int id;
-		// If the icon has "nt_" (night) prefix then remove it
-		if(icon.contains("nt_")) {
-			icon = icon.substring(3);
-		}
+//		// If the icon has "nt_" (night) prefix then remove it
+//		if(icon.contains("nt_")) {
+//			icon = icon.substring(3);
+//		}
+		
 		// need to check for the key before getting the value
 		if(iconHash.containsKey(icon)) {
 			id = iconHash.get(icon);
